d0cd404aadcdf1b5116690d8b883ea79ecef49ad
Akshat <75172495+Akshat-cs@users.noreply.github.com>
2023-06-12 15:51:07 +0530

    commit after next and previous buttons

---
+++ src/components/NewsItem.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export default class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3 ">
+        <div className="card ">
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ zindex: "1", left: "90%" }}
+          >
+            {source ? source : "Unknown"}
+          </span>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
